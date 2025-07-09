c3b98a49ad56b462bb538d2bdfcf5a67c17be4b1
황단우 <puppy@hwangdan-uui-MacBookPro.local>
2025-07-09 17:31:05 +0900

    secondver

--- app.py
+++ app.py
@@ -44,4 +44,4 @@ def reset():
     return "", 204
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5003, debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
